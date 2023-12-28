2ce1e5d8f2dfab34a11426f5eb1746f20999210b
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-28 18:45:40 +0530

    added course slice to redux store

---
+++ lms-frontend/src/redux/slices/courseSlices.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast"
+
+import axiosInstance from '../../config/axiosInstance'
+// to maintain the old state
+const initialState = {
+    courseList: []
+}
+// using async thunk
+
+export const getAllCourses = createAsyncThunk("/course/getAllCourses", async (data) => {
+    try {
+        const response = axiosInstance.get("/courses", data);
+        toast.promise(response, {
+            loading: 'Wait! fetching all courses',
+            success: (data)=> {
+                return data?.data?.message;
+            },
+            error: 'Failed to load courses'
+        });
+        return await response;
+    } catch (error) {
+        console.log(error);
+        toast.error(error?.response?.data?.message);
+    }
+})
+
+
+const courseSlice = createSlice({
+    name: "course",
+    initialState,
+    reducers: {},
+    extraReducers: () => {
+        
+    }
+})
+export default courseSlice.reducer;
+
+
