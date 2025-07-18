5bad312de2910d424d5ffbe6043b94874eb46c31
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-07-18 06:45:11 +0530

    Solved the version compatibility Issues

--- lms-frontend/tailwind.config.js
+++ lms-frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
@@ -7,7 +8,6 @@ export default {
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
-}
+  plugins: [daisyui],
+};
 
